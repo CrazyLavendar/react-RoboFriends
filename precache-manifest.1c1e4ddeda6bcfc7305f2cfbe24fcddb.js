724e75ddb5d271a157c9015f201b46485f77bab7
Jayamvimal <crazylavendar@gmail.com>
2020-09-16 01:17:57 +0530

    Updates

---
+++ precache-manifest.1c1e4ddeda6bcfc7305f2cfbe24fcddb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61a914cad21b940ed8b88ef685a76a7d",
+    "url": "/react-RoboFriends/index.html"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/react-RoboFriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "4de495039bd9389c0ba9",
+    "url": "/react-RoboFriends/static/css/main.d2c2034d.chunk.css"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/react-RoboFriends/static/js/2.cab51068.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-RoboFriends/static/js/2.cab51068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4de495039bd9389c0ba9",
+    "url": "/react-RoboFriends/static/js/main.7ffb8eb8.chunk.js"
+  },
+  {
+    "revision": "3ed63b600249ebccd2ff",
+    "url": "/react-RoboFriends/static/js/runtime-main.9c63d4bd.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/react-RoboFriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
